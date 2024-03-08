7ea6d4ac6b3a4e519de58ff592873f9b8655190b
Tsega Yaekob <tsegayaekob7@gmail.com>
2024-03-08 22:44:36 +0300

    home page search field

--- frontend/lib/presentation/screens/home.dart
+++ frontend/lib/presentation/screens/home.dart
@@ -1,15 +1,105 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 // ignore_for_file: use_key_in_widget_constructors
 
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
 
-class Home extends StatelessWidget {
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('data'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+          titleSpacing: 0,
+          title: Padding(
+            padding: const EdgeInsets.only(top: 30.0, left: 30),
+            child: Text(
+              'Ardi Travel',
+              style: TextStyle(
+                fontFamily: 'lobster',
+                fontSize: 26,
+                color: const Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(top: 25.0),
+              child: Container(
+                  width: 35,
+                  height: 35,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 0,
+                        blurRadius: 3,
+                        offset: Offset(1, 1),
+                      ),
+                    ],
+                  ),
+                  child: Center(child: Icon(Icons.request_page))),
+            ),
+            SizedBox(width: 20),
+          ],
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: Container(
+                margin: EdgeInsets.only(top: 30),
+                width: 325,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color:
+                          Color.fromARGB(255, 134, 134, 134).withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 1,
+                      offset: Offset(0, 0),
+                    ),
+                  ],
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Search',
+                            border: InputBorder.none,
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontFamily: 'cambo',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.search_rounded,
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+          ],
+        ));
   }
 }
